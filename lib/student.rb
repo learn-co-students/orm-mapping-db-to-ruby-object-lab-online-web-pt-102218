c5e0ee3a421a3be451ddbe19f728def16459d3e2
grant smith <grantsmith1256@gmail.com>
2019-01-17 23:26:10 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,72 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    instance = self.new
+    instance.id = row[0]
+    instance.name = row[1]
+    instance.grade = row[2]
+    instance
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    #binding.pry
+    sql = <<-SQL
+    SELECT * FROM students WHERE students.grade <= 11;
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT students.name FROM students WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql).map do |name|
+      self.new_from_db(name)
+    end
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
